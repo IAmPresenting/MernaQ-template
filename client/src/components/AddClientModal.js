bbd7e9c249d927200d1483a41dc956409f747119
mashaole <mashaolemogale@gmail.com>
2022-11-18 01:51:13 +0200

    fix button text

--- client/src/components/AddClientModal.js
+++ client/src/components/AddClientModal.js
@@ -3,32 +3,33 @@ import { set } from "mongoose";
 import { useState } from "react";
 import { FaUser } from "react-icons/fa";
 import { ADD_CLIENT } from "../mutations/clientMutations";
-import { GET_CLIENTS } from "../queries/clientQueries"
+import { GET_CLIENTS } from "../queries/clientQueries";
 
 export default function AddClientModal() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [phone, setPhone] = useState('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
 
   const [addClient] = useMutation(ADD_CLIENT, {
-    variables: { name, email, phone }, update(cache, { data: { deleteClient } }) {
+    variables: { name, email, phone },
+    update(cache, { data: { deleteClient } }) {
       const { clients } = cache.readQuery({ query: GET_CLIENTS });
       cache.writeQuery({
         query: GET_CLIENTS,
-        data: { clients: [...clients, addClient] }
-      })
-    }
-  })
+        data: { clients: [...clients, addClient] },
+      });
+    },
+  });
   const onSubmit = (e) => {
     e.preventDefault();
-    if (name === '' || email === '' || phone === '') {
-      return alert('Please fill in all fields')
+    if (name === "" || email === "" || phone === "") {
+      return alert("Please fill in all fields");
     }
     addClient(name, email, phone);
-    setName('');
-    setEmail('');
-    setPhone('');
-  }
+    setName("");
+    setEmail("");
+    setPhone("");
+  };
   return (
     <>
       <button
@@ -39,7 +40,7 @@ export default function AddClientModal() {
       >
         <div className="d-flex align-items-center">
           <FaUser className="icon" />
-          <div>Add Clinet</div>
+          <div>Add Client</div>
         </div>
       </button>
       <div
@@ -65,16 +66,38 @@ export default function AddClientModal() {
               <form onSubmit={onSubmit}>
                 <div className="mb-3">
                   <label className="form-lable">Name</label>
-                  <input type="text" className="form-control" id="name" value={name} onChange={(e) => setName(e.target.value)} />
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
                   <div className="mb-3">
                     <label className="form-lable">Email</label>
-                    <input type="text" className="form-control" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="email"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
                   </div>
                   <div className="mb-3">
                     <label className="form-lable">Phone</label>
-                    <input type="text" className="form-control" id="phone" value={phone} onChange={(e) => setPhone(e.target.value)} />
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="phone"
+                      value={phone}
+                      onChange={(e) => setPhone(e.target.value)}
+                    />
                   </div>
-                  <button type="Submit" data-bs-dismiss="modal" className="btn btn-primary">
+                  <button
+                    type="Submit"
+                    data-bs-dismiss="modal"
+                    className="btn btn-primary"
+                  >
                     Submit
                   </button>
                 </div>
